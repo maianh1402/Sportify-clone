3a20a2fb3ef7dc1ec74481854d85fd0d9dd51412
Mai Anh <maia2002td@gmail.com>
2023-07-27 17:31:25 +0700

    fix layout

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Library HUBT",
+  description: "Learn with HUBT!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
